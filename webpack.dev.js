9d186aee61f2d8566d9fbcc25d784e1919fed47e
quantum-corn <akashkhetan044@gmail.com>
2024-09-11 00:27:24 +0530

    Fix bugs

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,9 @@
-const common = require("webpack.common.js");
+const common = require("./webpack.common.js");
 const { merge } = require("webpack-merge");
 
 module.exports = merge(common, {
     mode: "development",
-    devtool: eval-source-map,
+    devtool: "eval-source-map",
     devServer: {
         watchFiles: ["./src/template.html"],
     },
